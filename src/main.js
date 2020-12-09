1a786d01bfa6871e79149b077ea4ec953abe00f7
skyscraper4 <jcardenas1@ucol.mx>
2020-12-09 16:45:09 -0600

    Implemented convertirAString

--- src/main.js
+++ src/main.js
@@ -7,7 +7,24 @@ export default class App {
         }
         return multiplicacion;
     }
+
+    convertirAString(numero) {
+    let z = 1;
+    let suma = "";
+        while(z <= numero) {
+        suma = suma + "*";
+          
+        z = z + 1;
+        }
+        return suma;
+    }
+
+    obtenerDivisibles(numero) {
+     
+ }
+
 }
 
 let app = new App();
 console.log(app.factorial(5));
+console.log(app.convertirAString(5));
