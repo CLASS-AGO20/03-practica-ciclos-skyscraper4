8bd37f5e8a2045455aef738cc17973c0d733f6f9
skyscraper4 <jcardenas1@ucol.mx>
2020-12-09 16:08:43 -0600

    Implemented factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {
+
+    factorial(numero) {
+      let multiplicacion = 1;
+        for(let i = numero; i > 0; i = i - 1) {
+         multiplicacion = multiplicacion * i;
+        }
+        return multiplicacion;
+    }
+}
 
 let app = new App();
+console.log(app.factorial(5));
